4beaf29b1735f4bb5b5e84dec8a003b54fe66ee0
Vansh Sharma <developer.viktorexe@gmail.com>
2025-04-21 10:01:01 +0530

    recommit

--- Lab 13 - Structures/question1.c
+++ Lab 13 - Structures/question1.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
 struct personal {
     char name[100];
     char doj[20];
     float salary;
 };
+
 int main() {
     struct personal p;
-    gets(p.name);
-    gets(p.doj);
+    
+    fgets(p.name, sizeof(p.name), stdin);
+    p.name[strcspn(p.name, "\n")] = '\0';
+    
+    fgets(p.doj, sizeof(p.doj), stdin);
+    p.doj[strcspn(p.doj, "\n")] = '\0';
+    
     scanf("%f", &p.salary);
+    
     printf("%s\n%s\n%.2f", p.name, p.doj, p.salary);
     return 0;
 }
--- Lab 13 - Structures/question2.c
+++ Lab 13 - Structures/question2.c
@@ -1,29 +1,47 @@
-
 #include <stdio.h>
 #include <string.h>
+
 struct student {
     int roll;
     char name[100];
     int m1, m2, m3, total;
 };
+
 void sort(struct student s[], int n) {
     struct student t;
-    for(int i=0;i<n-1;i++)
-        for(int j=i+1;j<n;j++)
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
             if(s[i].total < s[j].total) {
-                t = s[i]; s[i] = s[j]; s[j] = t;
+                t = s[i];
+                s[i] = s[j];
+                s[j] = t;
             }
+        }
+    }
 }
+
 int main() {
     struct student s[100];
     int n;
-    scanf("%d", &n);
-    for(int i=0;i<n;i++) {
-        scanf("%d %s %d %d %d", &s[i].roll, s[i].name, &s[i].m1, &s[i].m2, &s[i].m3);
+    
+    if(scanf("%d", &n) != 1 || n <= 0 || n > 100) {
+        printf("Invalid input for number of students.\n");
+        return 1;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        if(scanf("%d %99s %d %d %d", &s[i].roll, s[i].name, &s[i].m1, &s[i].m2, &s[i].m3) != 5) {
+            printf("Invalid input for student %d.\n", i + 1);
+            return 1;
+        }
         s[i].total = s[i].m1 + s[i].m2 + s[i].m3;
     }
-    sort(s,n);
-    for(int i=0;i<n;i++)
+    
+    sort(s, n);
+    
+    for(int i = 0; i < n; i++) {
         printf("%d %s %d\n", s[i].roll, s[i].name, s[i].total);
-    return 0;
     }
+    
+    return 0;
+}
